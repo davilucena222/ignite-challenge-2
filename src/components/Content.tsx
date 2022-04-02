dac768015971364daa4e75b2d541d5f0eb9eeed8
Davi Luiz <davilucena222@gmail.com>
2022-04-02 01:11:11 -0300

    second challenge finalized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,52 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+import { Header } from '../components/Header';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+}
+
+//tipando e desestruturando o selectedGenreId que é uma propriedade que está presente em outro estado, é possível "informar" para o componente Content que ele recebe essa propriedade mesmo que ela ainda não tenha sido repassada
+export function Content({selectedGenreId, selectedGenre} : ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId])
+
+  return (
+    <div className="container">
+        <Header selectedGenre={selectedGenre} />
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
